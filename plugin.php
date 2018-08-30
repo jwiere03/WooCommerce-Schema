7ffaba388bf23e2af514df105c088984d8bf7d81
SCS20180301A\Local Admin <jwierenga@scsgrandrapids.com>
2018-08-30 15:22:09 -0400

    Adding description, removing test lines that were commented out in the inital commit

--- plugin.php
+++ plugin.php
@@ -16,9 +16,6 @@ $json_price = $json_product->get_price();
 $json_instockstatus = $json_product->get_stock_status();
 $json_brand=get_bloginfo( 'name' );
 $json_image = get_the_post_thumbnail_url( $json_product->get_id(), 'full' );
-echo "This $json_image";
-//echo "<h1>$json_productTitle</h1><strong>Price:</strong> $json_price<br/>$json_description<br/>This product is: $json_instockstatus<br/><em>Product ID#: $json_productSKU</em><br/><br/>";
-//var_dump($json_product);
 echo '<script type="application/ld+json">
 {
   "@context": "http://schema.org/",
@@ -26,6 +23,7 @@ echo '<script type="application/ld+json">
     "brand": "'.$json_brand.'",
 	"name": "'.$json_productTitle.'",
 	"image": "'.$json_image.'",
+	"description": "'.$json_description.'",
 	"offers": {
 	"@type": "Offer",
 	"priceCurrency": "$",
